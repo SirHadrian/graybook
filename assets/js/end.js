909ffa0472791869942f702705a2dbb674c32720
Toma Adrian <sirhadrian21@gmail.com>
2023-10-03 00:28:32 +0300

    copy code tooltip

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,3 +3,9 @@ for (const notebook of document.getElementsByClassName('collapse')) {
         notebook.classList.toggle('open')
     })
 }
+
+for (const codeblock of document.querySelectorAll('pre')) {
+    const item = document.createElement("div")
+    item.innerHTML = "Click to copy";
+    codeblock.appendChild(item)
+}
