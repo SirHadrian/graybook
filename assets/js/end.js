dcfd9fd5e6e25af75334fee5b59f0d60fddd972a
Toma Adrian <sirhadrian21@gmail.com>
2023-10-03 01:24:47 +0300

    copy code block

--- assets/js/end.js
+++ assets/js/end.js
@@ -8,4 +8,15 @@ for (const codeblock of document.querySelectorAll('pre')) {
     const item = document.createElement("div")
     item.innerHTML = "Click to copy";
     codeblock.appendChild(item)
+
+    codeblock.addEventListener("click", () => {
+        codeblock.childNodes[1].innerHTML = ""
+        const el = document.createElement('textarea');
+        el.value = codeblock.textContent;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        codeblock.childNodes[1].innerHTML = "Copyed!"
+    })
 }
